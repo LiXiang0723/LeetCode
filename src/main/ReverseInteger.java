890e278e875450e5f24dbc36a546ebea7d31789f
Li Xiang <lixiang0723@gmail.com>
2017-02-18 10:24:46 -0800

    Add reverseInteger

---
+++ src/main/ReverseInteger.java
@@ -0,0 +1,27 @@
+package main;
+
+public class ReverseInteger {
+	 public int reverse(int x) {
+	        boolean isNegative = false;
+	        
+	        if(x<0){
+	            x= -x;
+	            isNegative = true;
+	        }
+	        
+	        long result = 0;
+	        while(x>0){
+	            int mod = x%10;
+	            x=x/10;
+	            result = result*10+mod;
+	        }
+	         
+	         if(result> Integer.MAX_VALUE){
+	            return 0;
+	         }
+	        if(isNegative){
+	            result = -result;
+	        }
+	     return (int)result;   
+	    }
+}
