f557c34d56b5c7995898e50745dacfeed77da1e9
Li Xiang <lixiang0723@gmail.com>
2017-03-04 11:20:38 -0800

    Update QuickSort()

--- src/implementations/QuickSort.java
+++ src/implementations/QuickSort.java
@@ -1,36 +1,36 @@
 package implementations;
 
 public class QuickSort {
-	 public void quickSort(int[] array,int lo, int hi){
-	        if(lo<hi){
-	            int pi = partition(array,lo,hi);
-	            quickSort(array,0,pi-1);
-	            quickSort(array,pi+1,hi);
-	        }
+    public void quickSort(int[] array, int lo, int hi) {
+	if (lo < hi) {
+	    int pi = partition(array, lo, hi);
+	    quickSort(array, 0, pi - 1);
+	    quickSort(array, pi + 1, hi);
+	}
+    }
+
+    private int partition(int[] array, int lo, int hi) {
+	int pivot = lo + (hi - lo) / 2;
+	int tmp = array[pivot];
+
+	// swap pivot with last element
+	array[pivot] = array[hi];
+	array[hi] = tmp;
+
+	int i = lo;
+
+	for (int j = lo; j < hi; j++) {
+	    if (array[j] < array[hi]) {
+		tmp = array[j];
+		array[j] = array[i];
+		array[i] = tmp;
+		i++;
 	    }
-	    
-	    private int partition(int[] array,int lo,int hi){
-	      int pivot = (hi-lo)/2;
-	      int tmp = array[pivot];
-	      
-	      //swap pivot with last element
-	      array[pivot] = array[hi];
-	      array[hi]=tmp;
-	      
-	      int i=0;
-	      
-	      for(int j =lo;j<hi;j++){
-	          if(array[j]<array[hi]){
-	              tmp = array[j];
-	              array[j] = array[i];
-	              array[i]=tmp;
-	              i++;
-	          }
-	      }
-	      tmp=array[hi];
-	      array[hi] = array[i];
-	      array[i] = tmp;
-	      
-	      return i;
-	    }
+	}
+	tmp = array[hi];
+	array[hi] = array[i];
+	array[i] = tmp;
+
+	return i;
+    }
 }
