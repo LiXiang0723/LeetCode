423cb520c8c70c8132275908472cb4da3da5b805
John L. Xiang <li.4.xiang@here.com>
2017-03-17 09:39:48 -0700

    Modified  LengthOfLongestSubString()

--- src/implementations/lengthOfLongestSubstring.java
+++ src/implementations/lengthOfLongestSubstring.java
@@ -1,28 +1,31 @@
 package implementations;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 //Time complexity O(n)
 public class lengthOfLongestSubstring {
 
-public int lengthOfLongestSubstring(String s) {
-    int size = s.length();
-    int result = 0;
-    int i=0;
-    int j=0;
-    List<Character> list = new ArrayList<Character>();
-    while(i<size&&j<size){
-      if(list.indexOf(s.charAt(j))==-1){
-          list.add(s.charAt(j));
-          j++;
-          result = Math.max(result,j-i);
-      }else{
-          list.remove(list.indexOf(s.charAt(i)));
-          i++;
-      }
+ public int lengthOfLongestSubstring(String s) {
+        int i =0;
+        int j =0;
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        int size = s.length();
+        int result = 0;
+        while(i<size && j<size){
+            if( map.containsKey(s.charAt(j))){
+                //Only updated i if the duplicated character'index is not smaller than i
+                if(map.get(s.charAt(j))>=i){
+                    i=map.get(s.charAt(j))+1;
+                }
+            }
+                map.put(s.charAt(j),j);
+                result = Math.max(result,j-i+1);
+                j++;
+        }
+        return result;
     }
-    return result;
-  }
 }
