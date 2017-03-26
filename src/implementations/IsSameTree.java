f7458a26a945a7290928ba3cc57c47e25c8bb235
Li Xiang <lixiang0723@gmail.com>
2017-03-25 21:51:06 -0700

    Add IsSameTree()

---
+++ src/implementations/IsSameTree.java
@@ -0,0 +1,21 @@
+package implementations;
+
+import common.TreeNode;
+
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+	if (p == null) {
+	    return q == null;
+	}
+
+	if (q == null) {
+	    return p == null;
+	}
+
+	if (p.val != q.val) {
+	    return false;
+	}
+
+	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
